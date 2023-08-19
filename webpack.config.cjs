707cf528fbf5bbd6dbae2037eba66e70edde7f0f
Oleksii Shepel <alex.s@paymentop.com>
2023-08-19 15:06:37 +0300

    added enviroment

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,21 +1,26 @@
-const babelConfig = require("./babel.config.json");
+const environment = require('./src/environments/environment');
+const babelConfig = require('./babel.config.json');
 
 module.exports = {
   resolve: {
-    extensions: [".js", ".ts"],
+    extensions: ['.js', '.ts'],
   },
 
   module: {
     rules: [
+      {
+        test: () => !environment.production,
+        sideEffects: true,
+      },
       {
         test: /\.[jt]s$/,
         /**
          * Exclude `node_modules` except the ones that need transpiling for IE11 compatibility.
          * Run `$ npx are-you-es5 check . -r` to get a list of those modules.
          */
-        exclude: "/node_modules",
+        exclude: '/node_modules',
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: Object.assign({}, babelConfig, {
             compact: false,
             cacheDirectory: true,
